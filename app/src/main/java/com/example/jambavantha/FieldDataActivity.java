cc7317f97c9a3db5a3752506dc1eb3bf5c18d214
Ankit7129 <ankit71297129@gmail.com>
2025-04-11 22:05:51 +0530

    pushed after adding ai api response

--- app/src/main/java/com/example/jambavantha/FieldDataActivity.java
+++ app/src/main/java/com/example/jambavantha/FieldDataActivity.java
@@ -23,7 +23,7 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.URL;
+
 
 public class FieldDataActivity extends AppCompatActivity {
 
